f04f1b40aec3f93c4717361252ad425dd4bfa133
Eric20Junior <eric20junior@gmail.com>
2021-11-18 11:08:58 -0800

    0x02-python-import_modules

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,24 +1,45 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    import sys
+    import sys import agrv
     from calculator_1 import add, sub, mul, div
-    argv = sys.argv[1:]
-    argv_count = len(argv)
-    operators = ["+", "-", "*", "/"]
-    if argv_count is not 3:
-        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-        exit(1)
-    elif sys.argv[2] not in operators:
-        print("Unknown operator. Available operators: +, -, * and /")
-        exit(1)
-    else:
-        a = int(sys.argv[1])
-        b = int(sys.argv[3])
-        if sys.argv[2] is "+":
-            print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
-        elif sys.argv[2] is "-":
-            print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
-        elif sys.argv[2] is "*":
-            print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
-        elif sys.argv[2] is "/":
-            print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))
+    count = len(argv)
+
+    if count != 4:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>".format(argv[0])
+                exit(1)
+
+                num1 = int(argv[1])
+                num2 = int(argv[3])
+                op = argv[2]
+
+                def not_found():
+                    print("Unknown operator. Available operators: +, -, * and /")
+                    exit(1)
+
+                    def add():
+                    toatl =add(num1, num2)
+                    print("{:d} + {:d}".format(num1, num2, total)))
+                    return total
+
+                def sub():
+                    toatl =sub(num1, num2)
+                    print("{:d} - {:d}".format(num1, num2, total)))
+                    return total
+
+                def mul():
+                    toatl =mul(num1, num2)
+                    print("{:d} * {:d}".format(num1, num2, total)))
+                    return total
+
+                def div():
+                    toatl =mul(num1, num2)
+                    print("{:d} / {:d}".format(num1, num2, total)))
+                    return total
+
+                options = {
+                        "+": add,
+                        "-": sub,
+                        "*": mul,
+                        "/": div,
+                        }
+                options.get(op, not_found)()
