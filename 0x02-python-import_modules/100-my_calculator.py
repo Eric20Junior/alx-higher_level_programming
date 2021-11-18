60964bbfac80a255dd8adb746e512df73ee692b2
Eric20Junior <eric20junior@gmail.com>
2021-11-18 11:23:30 -0800

    0x02-python-import_modules

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,45 +1,22 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    import sys import agrv
     from calculator_1 import add, sub, mul, div
-    count = len(argv)
+    from sys import argv
+    my_calc()
 
-    if count != 4:
-        print("Usage: ./100-my_calculator.py <a> <operator> <b>".format(argv[0])
-                exit(1)
+def my_calc():
+    operations = {"+": add, "-": sub, "*": mul, "/": div}
+    length = len(argv) - 1
+    if (length < 3):
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        exit(1)
 
-                num1 = int(argv[1])
-                num2 = int(argv[3])
-                op = argv[2]
+    a = int(argv[1])
+    b = int(argv[3])
+    operator = argv[2]
 
-                def not_found():
-                    print("Unknown operator. Available operators: +, -, * and /")
-                    exit(1)
-
-                    def my_add():
-                    toatl = add(num1, num2)
-                    print("{:d} + {:d}".format(num1, num2, total)))
-                    return total
-
-                def my_sub():
-                    toatl = sub(num1, num2)
-                    print("{:d} - {:d}".format(num1, num2, total)))
-                    return total
-
-                def my_mul():
-                    toatl = mul(num1, num2)
-                    print("{:d} * {:d}".format(num1, num2, total)))
-                    return total
-
-                def my_div():
-                    toatl = div(num1, num2)
-                    print("{:d} / {:d}".format(num1, num2, total)))
-                    return total
-
-                options = {
-                        "+": my_add,
-                        "-": my_sub,
-                        "*": my_mul,
-                        "/": my_div,
-                        }
-                options.get(op, not_found)()
+    if operator not in operations:
+        print("Unknown operator. Available operators: +, -, * and /")
+        exit(1)
+    result = operations[operator](a, b)
+    print("{} {} {} = {}".format(a, operator, b, result))
