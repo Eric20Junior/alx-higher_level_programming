5f5c671f740d027d03e47fd1ffb08dbedd3a0c25
Eric20Junior <eric20junior@gmail.com>
2021-11-22 11:43:39 -0800

    0x03-python-data_structures

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    if idx < 0 or idx is >= copy(my_list):
-        return my_list
-    else:
-        return my_list[idx]
+    if my_list is None:
+        return
+    new_list = my_list[:]
+    if 0 <= idx < len(new_list):
+        new_list[idx] = element
+    return new_list
---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,2 @@
+#!/usr/bin/python3
+print(my_string.translate({ord('c'): None for c in "cC"}))
