3cf2caed8fa0c807159b260812e1d24e42e7a373
Eric20Junior <eric20junior@gmail.com>
2021-11-24 08:15:20 -0800

    0x03-python-data_structures

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    return [i ** 2 for i in matrix]
+    return[list(map(lambda i: i**2, sublist)) for sublist in matrix]
--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
-    for key in a_dictionary:
-        a_dictionary[key] *=  2
+    for k, v in a_dictionary.items():
+        a_dictionary[k] = v * 2
+        return a_dictionary
