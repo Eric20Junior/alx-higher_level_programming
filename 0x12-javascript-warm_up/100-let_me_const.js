b1b34d0123918bf46c805c55ef3218a9ff5bf0dd
Eric20Junior <eric20junior@gmail.com>
2022-02-28 16:03:33 -0800

    javascript warm up

---
+++ 0x12-javascript-warm_up/100-let_me_const.js
@@ -0,0 +1,3 @@
+#!/usr/bin/node
+// override in 100-main.js
+myVar = 333;
---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+
+const string = ["C is fun”, “Python is cool”, “JavaScript is amazing”];
+for (const 1 of string) {
+	console.log(1);
+}
