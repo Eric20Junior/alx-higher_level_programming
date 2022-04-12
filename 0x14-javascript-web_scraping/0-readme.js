c5ac9aa2faadab77da001b0e9e9884d0726570e4
Eric20Junior <eric20junior@gmail.com>
2022-04-12 21:53:43 +0100

    update

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -4,8 +4,8 @@ let filename = process.argv[2];
 const fs = require('fs');
 fs.readFile(filename, 'utf8', function (err, data) {
   if (err) {
-    console.log(err);
+    return console.log(err);
   } else {
-    console.log(data);
+    process.stdout.write(data);
   }
 });
